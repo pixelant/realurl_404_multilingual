eb187baf45e3c8a2cdf8cf8bb6f1220013655194
Alex Bachynskyi <alex@pixelant.se>
2017-10-25 16:03:40 +0300

    [FEATURE] add possibility to redirect to first domain record

--- Classes/Hooks/FrontendHook.php
+++ Classes/Hooks/FrontendHook.php
@@ -45,6 +45,11 @@ class FrontendHook
      */
     const MODE_REDIRECT = '2';
 
+    /**
+     * use first domain
+     */
+    const MODE_FIRST_DOMAIN = '1';
+
 
     /**
      * host
@@ -100,7 +105,6 @@ class FrontendHook
             echo $this->getProvisionally404Page();
             exit();
         }
-
         $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['realurl_404_multilingual']);
         $currentUrl = $params['currentUrl'];
         $reasonText = $params['reasonText'];
@@ -109,28 +113,32 @@ class FrontendHook
         $statusCode = $GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFound_handling_statheader'];
 
         if (isset($pageAccessFailureReasons['fe_group']) && array_shift($pageAccessFailureReasons['fe_group']) != 0) {
-
             $unauthorizedPage = $this->config['unauthorizedPage'];
             $unauthorizedPage = (!$unauthorizedPage ? '401' : $unauthorizedPage);
-            $destinationUrl = $this->getDestinationUrl($currentUrl, $unauthorizedPage);
+            $destinationUrl = $this->getDestinationUrl($currentUrl, $unauthorizedPage, $extConf);
             $destinationUrl .= "?return_url=".urlencode($currentUrl)."&tx_realurl404multilingual=1";
             //$header = "HTTP/1.0 401 Unauthorized";
             $mode = self::MODE_REDIRECT; // force redirect
         } else {
-
             // define the page name
             $errorpage = $this->config['errorPage'];
             $errorpage = ($errorpage == '' ? '404' : $errorpage);
-            $destinationUrl = $this->getDestinationUrl($currentUrl, $errorpage);
+            $destinationUrl = $this->getDestinationUrl($currentUrl, $errorpage, $extConf);
         }
-
 
         switch ($mode) {
             case self::MODE_REDIRECT:
                 HttpUtility::redirect($destinationUrl, HttpUtility::HTTP_STATUS_301);
                 break;
             default:
-                $this->getPageAndDisplay($destinationUrl, ($statusCode ? $statusCode : "HTTP/1.0 404 Not Found"));
+                if(self::MODE_FIRST_DOMAIN == $extConf['firstDomain'] && !$this->hostEqualFirstDomain())
+                {
+                    $destinationUrl = $this->getDestinationUrl('', $this->getUri($currentUrl), $extConf, TRUE);
+                    HttpUtility::redirect($destinationUrl, HttpUtility::HTTP_STATUS_301);
+                }else
+                {
+                    $this->getPageAndDisplay($destinationUrl, ($statusCode ? $statusCode : "HTTP/1.0 404 Not Found"));
+                }
                 break;
         }
     }
@@ -216,12 +224,25 @@ class FrontendHook
     /**
      * @param string $currentUrl
      * @param string $suffix
+     * @param array $extConf
+     * @param bool $extConf
      * @return string
      */
-    private function getDestinationUrl($currentUrl = "", $suffix)
+    private function getDestinationUrl($currentUrl = "", $suffix, $extConf, $useFirstDomain = FALSE)
     {
-
         $host = \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('HTTP_HOST');
+        // Get page Uid of start page
+        $pageUid = $this->getDomainStartPage($host, GeneralUtility::getIndpEnv('SCRIPT_NAME'), GeneralUtility::getIndpEnv('REQUEST_URI'));
+        // Get rootline
+        $rootLine = \TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine($pageUid);
+        // Get first domain record
+        $firstDomain = \TYPO3\CMS\Backend\Utility\BackendUtility::firstDomainRecord($rootLine);
+        
+        // Change host to first domain record if 'firstDomain' flag is set
+        if(self::MODE_FIRST_DOMAIN == $extConf['firstDomain'] && $useFirstDomain)
+        {   
+            $host = $firstDomain;
+        }
 
         $uri = $this->getUri($currentUrl);
         list($script, $option) = explode("?", $uri);
@@ -287,6 +308,60 @@ class FrontendHook
 
         return ((!empty($useHttps) && $useHttps !== 'off') ? "https" : "http") . "://" . implode("/", $url_array);
     }
+
+    /**
+     * Return TRUE if http_host and first domain record is equal
+     *
+     * @return boolean
+     */
+    protected function hostEqualFirstDomain()
+    {
+        // Get host
+        $host = \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('HTTP_HOST');
+        // Get page Uid of start page
+        $pageUid = $this->getDomainStartPage($host, GeneralUtility::getIndpEnv('SCRIPT_NAME'), GeneralUtility::getIndpEnv('REQUEST_URI'));
+        // Get rootline
+        $rootLine = \TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine($pageUid);
+        // Get first domain record
+        $firstDomain = \TYPO3\CMS\Backend\Utility\BackendUtility::firstDomainRecord($rootLine);
+
+        return ($host == $firstDomain) ? TRUE : FALSE;
+    }
+
+    /**
+     * Will find the page carrying the domain record matching the input domain.
+     * Will not exit even if it found domain record instructs to do so.
+     *
+     * @param string $domain Domain name to search for. Eg. "www.typo3.com". Typical the HTTP_HOST value.
+     * @param string $path Path for the current script in domain. Eg. "/somedir/subdir". Typ. supplied by \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('SCRIPT_NAME')
+     * @param string $request_uri Request URI: Used to get parameters from if they should be appended. Typ. supplied by \TYPO3\CMS\Core\Utility\GeneralUtility::getIndpEnv('REQUEST_URI')
+     * @return mixed If found, returns integer with page UID where found. Otherwise blank. Will not exit even if location-header is sent.
+     * @see \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController::findDomainRecord()
+     */
+    protected function getDomainStartPage($domain, $path = '', $request_uri = '')
+    {
+        // Get PageRepository instance
+        $pageRepoInstance = GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Page\\PageRepository');
+        
+        $domain = explode(':', $domain);
+        $domain = strtolower(preg_replace('/\\.$/', '', $domain[0]));
+        // Removing extra trailing slashes
+        $path = trim(preg_replace('/\\/[^\\/]*$/', '', $path));
+        // Appending to domain string
+        $domain .= $path;
+        $domain = preg_replace('/\\/*$/', '', $domain);
+
+        $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('pages.uid', 'pages,sys_domain', 'pages.uid=sys_domain.pid
+                        AND sys_domain.hidden=0
+                        AND (sys_domain.domainName=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($domain, 'sys_domain') . ' OR sys_domain.domainName=' . $GLOBALS['TYPO3_DB']->fullQuoteStr(($domain . '/'), 'sys_domain') . ') ' . $pageRepo->where_hid_del . $pageRepo->where_groupAccess, '', '', 1);
+        $row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+        $GLOBALS['TYPO3_DB']->sql_free_result($res);
+        if ($row) {
+            return $row['uid'];
+        }
+        return '';
+    }
+
 
 
     /**
